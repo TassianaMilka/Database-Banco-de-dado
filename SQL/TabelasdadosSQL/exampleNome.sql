9858087912a0b6cb0fb7893000aac4473bc49b0d
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-29 18:23:15 -0300

    Create exampleNome.sql

---
+++ SQL/TabelasdadosSQL/exampleNome.sql
@@ -0,0 +1,13 @@
+--Criação da tabela e o nome dela--
+
+CREATE TABLE exampleNome(
+ /* id INTEGER PRIMARY KEY,*/
+--O nome em formato de texto(Text de não nulo)--
+  one TEXT VARCHAR(100) NOT NULL,
+  two  FLOAT 
+ 
+
+);
+--Inserir os dados da tabela de informação--
+
+INSERT INTO exampleNome VALUES ('Mark', 1900000);
