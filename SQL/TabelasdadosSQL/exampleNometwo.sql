aa4670e5a6e5568d6be5ae81ca4319d3503a02f3
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-29 18:23:41 -0300

    Create  exampleNometwo.sql

---
+++ SQL/TabelasdadosSQL/exampleNometwo.sql
@@ -0,0 +1,13 @@
+--Criação da tabela e o nome dela--
+
+CREATE TABLE exampleNometwo(
+ /* id INTEGER PRIMARY KEY,*/
+--O nome em formato de texto(Text de não nulo)--
+  one TEXT VARCHAR(100) NOT NULL,
+  two  FLOAT 
+ 
+
+);
+--Inserir os dados da tabela de informação--
+
+INSERT INTO exampleNometwo VALUES ('Jhon', 1900000);
