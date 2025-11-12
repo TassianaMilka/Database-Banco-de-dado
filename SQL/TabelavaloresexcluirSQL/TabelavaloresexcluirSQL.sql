938bc9ba2ae86ae4ff748a143103bb7de4545f0a
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-11-12 02:10:31 -0300

    Update TabelavaloresexcluirSQL.sql

--- SQL/TabelavaloresexcluirSQL/TabelavaloresexcluirSQL.sql
+++ SQL/TabelavaloresexcluirSQL/TabelavaloresexcluirSQL.sql
@@ -3,7 +3,7 @@
 
 --Criação da tabela e o nome dela--
 
-CREATE TABLE exampleNome(
+CREATE TABLE exampleValor(
  /* id INTEGER PRIMARY KEY,*/
 --O nome em formato de texto(Text de não nulo)--
   one TEXT VARCHAR(100) NOT NULL,
@@ -13,9 +13,9 @@ CREATE TABLE exampleNome(
 );
 --Inserir os dados da tabela de informação--
 
-INSERT INTO exampleNome VALUES ('R$900.00', 'R$19.000.00');
-INSERT INTO exampleNome VALUES ('R$890.00', 'R$123.00');
+INSERT INTO exampleValor VALUES ('R$900.00', 'R$19.000.00');
+INSERT INTO exampleValor VALUES ('R$890.00', 'R$123.00');
 
 
 -- Excluir a tabela
-DROP TABLE exampleNome;
+DROP TABLE exampleValor;
